17b8b56472b84f84780a67345aa0c285996776ea
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-25 15:01:36 +0200

    fixed handling protecting routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/generate-program", "/profile"]);
+const isProtectedRoute = createRouteMatcher([
+  "/generate-program(.*)",
+  "/profile(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
