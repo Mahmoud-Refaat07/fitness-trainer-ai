adabe36f9a6b67905c228e5d90acdd82618e2495
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-25 14:59:12 +0200

    implemented authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      Home Page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default page;
